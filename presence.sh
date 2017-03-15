d6add973dfa79e2696a5a35cdda2bf0f4de39ca2
AP\AsifMoha <mohammad.asif@in.unisys.com>
2017-03-15 15:48:12 +0530

    adding presence.sh file

---
+++ presence.sh
@@ -0,0 +1,2 @@
+git add-symlink ../../DEPENDENCIES/osp/signalling/smmap-ssmap.xml PRESENCE-SERVICE/UNISYS/lib/smmap-ssmap.xml
+git add-symlink ../../DEPENDENCIES/osp/signalling/smmap-ssmap PRESENCE-SERVICE/UNISYS/lib/smmap-ssmap
